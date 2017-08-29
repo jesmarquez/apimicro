2a3cbea485ebf496ecc718cd31233b6146fb08a0
jesmarquez <jamarquez@uao.edu.co>
2017-08-29 13:24:42 -0500

    Crea test - conecta con micro

--- test/api-test.js
+++ test/api-test.js
@@ -1,11 +1,27 @@
 import test from 'ava'
+import micro, { send } from 'micro'
+import listen from 'test-listen'
+import request from 'request-promise'
+import uuid from 'uuid-base62'
 
 test('foo', t => {
   t.pass()
 })
 
 test('bar', async t => {
-    const bar = Promise.resolve('bar');
- 
-    t.is(await bar, 'bar');
+  const bar = Promise.resolve('bar')
+
+  t.is(await bar, 'bar')
+})
+
+test('GET /:id', async t => {
+  let id = uuid.v4()
+
+  let srv = micro(async (req, res) => {
+    send(res, 200, { id })
+  })
+
+  let url = await listen(srv)
+  let body = await request({ uri: url, json: true })
+  t.deepEqual(body, { id })
 })
