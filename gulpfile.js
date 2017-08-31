5c6d5e1796f3a37df4627430bdc3428aad06e95e
Jesus Marquez <jesmqz@gmail.com>
2017-08-30 22:42:31 -0500

    Crea endpoint y test users

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,36 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
+const gulp = require('gulp')
+const babel = require('gulp-babel')
 
 gulp.task('microservices', () =>
-    gulp.src(['api.js', 'auth.js', 'config.js'])
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(gulp.dest('dist'))
-);
+  gulp.src(['config.js', 'api.js', 'auth.js', 'users.js'])
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(gulp.dest('dist'))
+)
 
 gulp.task('utils', () =>
-    gulp.src('./lib/utils.js')
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(gulp.dest('dist/lib'))
-);
+  gulp.src('./lib/utils.js')
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(gulp.dest('dist/lib'))
+)
 
 gulp.task('fixtures', () =>
-    gulp.src('./test/fixtures/index.js')
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(gulp.dest('dist/test/fixtures'))
-);
+  gulp.src('./test/fixtures/index.js')
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(gulp.dest('dist/test/fixtures'))
+)
 
 gulp.task('stub', () =>
-    gulp.src('./test/stub/db.js')
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(gulp.dest('dist/test/stub'))
-);
+  gulp.src('./test/stub/db.js')
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(gulp.dest('dist/test/stub'))
+)
 
-gulp.task('default', [ 'microservices', 'utils', 'fixtures','stub']);
+gulp.task('default', ['microservices', 'utils', 'fixtures', 'stub'])
