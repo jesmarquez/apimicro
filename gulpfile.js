4197a0735c5f6aa4ecbd77239474b805aff4e59f
Jesus Marquez <jesmqz@gmail.com>
2017-08-30 21:57:17 -0500

    Automatiza transpilacion

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+gulp.task('microservices', () =>
+    gulp.src(['api.js', 'auth.js', 'config.js'])
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('dist'))
+);
+
+gulp.task('utils', () =>
+    gulp.src('./lib/utils.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('dist/lib'))
+);
+
+gulp.task('fixtures', () =>
+    gulp.src('./test/fixtures/index.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('dist/test/fixtures'))
+);
+
+gulp.task('stub', () =>
+    gulp.src('./test/stub/db.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('dist/test/stub'))
+);
+
+gulp.task('default', [ 'microservices', 'utils', 'fixtures','stub']);
